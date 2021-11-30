f4444d8bf0ebf95beb9897a13e488570c416f8da
Kathryn Reeve <67553+BinaryKitten@users.noreply.github.com>
2021-11-30 09:44:43 +0000

    feature/improve-artisan-command - add config verify command and update config

---
+++ src/Commands/LaravelShortenUrlsVerifyConfigCommand.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CarAndClassic\LaravelShortenUrls\Commands;
+
+use CarAndClassic\LaravelShortenUrls\Contracts\UrlShorteningService;
+use CarAndClassic\LaravelShortenUrls\Services\Bitly;
+use Illuminate\Console\Command;
+use Illuminate\Contracts\Container\BindingResolutionException;
+use Illuminate\Support\Str;
+
+class LaravelShortenUrlsVerifyConfigCommand extends Command
+{
+    /** @var string $signature */
+    protected $signature = 'url:verify-shorten-config {provider=default}';
+
+    /** @var string $description */
+    protected $description = 'Verify the configuration for a URL Shortening service';
+
+    public function handle(): int
+    {
+        if (!config()->has('shorten-urls')) {
+            $this->error('Shorten Urls configuration is missing or not compiled');
+            return self::FAILURE;
+        }
+
+        if (
+            // since by default, missing keys return null, we can skip the has check
+            !is_string(config('shorten-urls.provider'))
+            || empty(config('shorten-urls.provider'))
+        ) {
+            $this->error('Shorten Urls configuration provider key (used for default) is missing or empty');
+            return self::FAILURE;
+        }
+
+        $providerList = config('shorten-urls.provider-list');
+        if (
+            // since by default, missing keys return null, we can skip the has check
+            !is_array($providerList)
+            || empty($providerList)
+        ) {
+            $this->error('Shorten Urls configuration provider configuration list is missing or empty');
+            return self::FAILURE;
+        }
+
+        $providerName = $this->argument('provider');
+        if (strtolower($providerName) === 'default') {
+            $providerName = config('shorten-urls.provider');
+        }
+
+        if (!array_key_exists($providerName, $providerList)) {
+            $this->error('Shorten Urls configuration is missing configuration for ' . $providerName);
+            return self::FAILURE;
+        }
+
+        $providerConfig = $providerList[$providerName];
+        $serviceClass = $providerConfig['service_class']
+            ?: ('CarAndClassic\\LaravelShortenUrls\\Services\\' . Str::studly($providerName));
+
+        $service = $this->getLaravel()->make($serviceClass);
+
+        if (!$service instanceof UrlShorteningService) {
+            $this->error(
+                'Shorten Urls configuration Service Class for ' . $providerName . ' cannot be created from Container'
+            );
+            return self::FAILURE;
+        }
+
+        $result = $service->verifyConfig();
+        if ($result === true) {
+            $this->info('Shorten Urls configuration for ' . $providerName . ' passed verification needed to create basic short urls');
+            return self::SUCCESS;
+        }
+
+        return self::SUCCESS;
+    }
+}
